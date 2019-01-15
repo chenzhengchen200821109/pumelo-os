5b24c6f17a63df77157f9d4e70e89f0d382e0dfe
Joey Chen <987102818@qq.com>
2019-01-16 04:32:20 +0800

    update 20190116

---
+++ os/include/kernel.h
@@ -0,0 +1,23 @@
+#ifndef _KERNEL_H
+#define _KERNEL_H
+
+#define RPL0 0
+#define RPL3 3
+
+#define GDT_TI 0
+
+#define SELECTOR_KERNEL_CODE ((0x1 << 3) + (GDT_TI << 2) + RPL0)
+#define SELECTOR_KERNEL_DATA ((0x2 << 3) + (GDT_TI << 2) + RPL0)
+#define SELECTOR_KERNEL_STACK SELECTOR_KERNEL_DATA
+#define SELECTOR_KERNEL_GS ((0x3 << 3) + (GDT_TI << 2) + RPL0)
+
+/*------------------ IDT descriptor attributes ------------------*/
+#define IDT_DESC_P 1
+#define IDT_DESC_DPL0 0
+#define IDT_DESC_DPL3 3
+#define IDT_DESC_32_TYPE 0xe
+
+#define IDT_DESC_ATTR_DPL0 ((IDT_DESC_P << 7) + (IDT_DESC_DPL0 << 5) + IDT_DESC_32_TYPE)
+#define IDT_DESC_ATTR_DPL3 ((IDT_DESC_P << 7) + (IDT_DESC_DPL3 << 5) + IDT_DESC_32_TYPE)
+
+#endif
---
+++ os/include/stdio.h
@@ -0,0 +1,20 @@
+#ifndef _STDIO_H__
+#define _STDIO_H__
+
+#include "defs.h"
+#include "stdarg.h"
+
+/* kernel/stdio.c */
+int kprintf(const char *fmt, ...);
+int kvprintf(const char *fmt, va_list ap);
+void kputchar(int c);
+int kputs(const char *str);
+
+/* kernel/printfmt.c */
+void printfmt(void (*putch)(int, void *), void *putdat, const char *fmt, ...);
+void vprintfmt(void (*putch)(int, void *), void *putdat, const char *fmt, va_list ap);
+int snprintf(char *str, size_t size, const char *fmt, ...);
+int vsnprintf(char *str, size_t size, const char *fmt, va_list ap);
+
+#endif /* _STDIO_H__ */
+
