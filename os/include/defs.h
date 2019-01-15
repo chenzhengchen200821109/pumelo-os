716a376f8dcda26233072877ee95493f63f3f597
Joey Chen <987102818@qq.com>
2019-01-16 04:30:22 +0800

    update 20190116

--- os/include/defs.h
+++ os/include/defs.h
@@ -1,6 +1,7 @@
 #ifndef _DEFS_H
 #define _DEFS_H
 
+/* Explicitly-sized versions of integer types */
 typedef signed char int8_t;
 typedef signed short int int16_t;
 typedef signed int int32_t;
@@ -11,6 +12,14 @@ typedef unsigned int uint32_t;
 typedef unsigned long long int uint64_t;
 
 typedef uint32_t size_t;
+
+/*
+ * Pointers and addresses are 32 bits long.
+ * We use pointer types to represent addresses,
+ * uintptr_t to represent the numerical values of addresses.
+ */
+typedef int32_t intptr_t;
+typedef uint32_t uintptr_t;
 
 #define NULL 0
 
--- os/kernel/main.c
+++ os/kernel/main.c
@@ -1,22 +1,26 @@
 #include "console.h"
+#include "intr.h"
+#include "stdio.h"
 
 int main()
 {
     // console initialization
     cons_init();
+    idt_init();
 
     //while (1);
-    cons_putc('k');
-    cons_putc('e');
-    cons_putc('r');
-    cons_putc('n');
-    cons_putc('e');
-    cons_putc('l');
-    cons_putc('\n');
-    cons_putc('1');
-    cons_putc('2');
-    cons_putc('\b');
-    cons_putc('3');
+    //cons_putc('k');
+    //cons_putc('e');
+    //cons_putc('r');
+    //cons_putc('n');
+    //cons_putc('e');
+    //cons_putc('l');
+    //cons_putc('\n');
+    //cons_putc('1');
+    //cons_putc('2');
+    //cons_putc('\b');
+    //cons_putc('3');
+    kputs("kernel");
     while (1)
         ;
     return 0;
